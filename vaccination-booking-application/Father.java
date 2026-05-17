a0c97cedd6e12699aead512d988468d3fa26be4c
ABHISHEK  VERMA <76154794+abhishekverma123@users.noreply.github.com>
2026-05-18 02:31:32 +0530

    Add files via upload
    SPRING IOC + DI Using  Constructor Dependency Injection

---
+++ vaccination-booking-application/Father.java
@@ -0,0 +1,93 @@
+package com.example.Vaccination;
+
+import com.example.Vaccination.User;
+
+/**
+  This class is an implementation of a User Interface based on the selection 
+  in the console the user type is selected.You need to complete this class 
+  based on the following tasks.
+    
+   Tasks:
+ a.Override the methods of User Interface.
+ b.Adding common attributes:
+    1. String name
+    2. Integer age
+    3. boolean isVaccinated
+    4. TimeAndLocation(class), vaccine(interface), both are injected by the constructor method.
+    5. The arguments of the constructor for all users should be like "public User(TimeAndLocation timeAndLocation, Vaccine vaccine)"
+ c.Build the logic for all the methods based on the description mentioned in the User Interface.
+ 
+**/
+public class Father implements User {
+        private String name;
+        private int age;
+       private  boolean isVaccinated;
+        
+        
+        // reference of  Vaccine interface and timeAndLocation class
+        private TimeAndLocation  timeAndLocation;
+        private Vaccine vaccine;
+        
+            public Father(TimeAndLocation timeAndLocation, Vaccine vaccine){
+            	this.timeAndLocation = timeAndLocation;	
+            	this.vaccine = vaccine;
+            	 this.isVaccinated = false;
+            	
+            }
+//            @Override
+//            public void setUserTimeLocationDetails(String timeSlot, String location, String date) {
+//    	        this.timeSlot = timeSlot;
+//    	        this.location = location;
+//    	        this.date = date;
+//       }
+            
+	@Override
+	public Vaccine getVaccineDetails() {
+		// TODO Auto-generated method stub
+		return this.vaccine;
+	}
+
+	@Override
+	public void setUserDetails(String name, int age, TimeAndLocation timeAndLocation) {
+		// TODO Auto-generated method stub
+		this.name = name;
+		this.age = age;
+		this.timeAndLocation = timeAndLocation;	
+	}
+
+	@Override
+	public void setAppointment() {
+		
+		// TODO Auto-generated method stub
+		
+		/** This function updates vaccination status of the user and prints appointment details in the console with includes user name , Vaccine name and lastly timeAndLocation class based details 
+        For example:  "Hello John your appointment has been fixed for Covid Vaccine on  12 Pm at Mumbai  on 2023-05-10". 
+     **/
+		if(!isVaccinated) {
+			isVaccinated = true;
+		System.out.println("Hello "+this.name+" your appointment has been fixed for "+this.getVaccineDetails().getType()+" Vaccine on "+this.timeAndLocation.getDetails());
+		}
+		else {
+			System.out.println("User is already Vaccinated");
+		}
+		
+	}
+
+	@Override
+	public boolean IsVaccinated() {
+		if(isVaccinated) {
+			  return true;
+		}
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+	
+	/** This function returns a boolean type indicating the vaccination status of a user. If the user is vaccinated it should return true else false. **/
+
+
+}
---
+++ vaccination-booking-application/VaccinationApplication.java
@@ -0,0 +1,232 @@
+package com.example.Vaccination;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class VaccinationApplication {
+
+   
+	public static void main(String[] args) {
+
+        /*
+		You need to complete this application as mentioned in the problem 
+		statement build your own logic and perform the following tasks.
+
+		 Tasks:
+		1. Fetch context from ApplicationContext.xml and initiate Scanner.
+		2. Fetch vaccine and User type choice.
+		3. Get the required bean from context.
+		4. Get the appointment details form user
+		5. Display the appointment details
+		6. Run the loop again to book for another user or else exit.
+		 */
+
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+    	   
+    	    // Prompt of Application
+    	
+    	System.out.println("Welcome to the Vaccination Application");
+    	Scanner scanner = new Scanner(System.in);
+    	//User user = (User) context.getBean()
+    	
+    	 while(true) {
+    		 System.out.println("Please choose your vaccine preference:");
+    		 
+    		 System.out.println("1. Covid");
+    		 System.out.println("2. Polio");
+    		 System.out.println("3. Typhoid");
+    		 int choosePreference = scanner.nextInt();
+    		 
+    		 scanner.nextLine();
+    		  String userName = "";
+    		  String userType = "";
+    		  
+    		 System.out.println("Whom do you want to vaccinate");
+  
+    		 System.out.println("1. Father");
+    		 System.out.println("2. Mother");
+    		 System.out.println("3. Spouse");
+    		 System.out.println("4. Self");
+    		 System.out.println("5. Exit");
+    		 int chooseVaccinatePerson = scanner.nextInt();
+    		 scanner.nextLine();
+    		 
+    		 switch(choosePreference) {
+    		 case 1 : {
+    			     userType = "Covid"; 
+    			 switch(chooseVaccinatePerson) {
+    			 
+    			 case 1:{ 
+    				 userName = "father";
+    			            break;
+    			 }
+    		 
+    			 case 2:{ 
+    				 userName = "mother";
+			            break;
+			 }
+    		 
+    		 case 3: {
+    		 
+    			 userName = "spouse";
+		            break;
+		 }
+    		 
+    		 case 4: {
+    			
+
+    			 userName = "self";
+		            break;  
+    		 }
+    		 default : {
+    			     
+    			    return;
+    		 }
+    			 }
+    			 break;
+    		 
+    		 }
+    		 
+    		 case 2 : {
+    			 userType = "Polio";
+                     switch(chooseVaccinatePerson) {
+    			 
+    			 case 1:{
+    			           
+    				 userName = "father";
+    			            break;
+    		 
+    			 }
+    		 
+    
+    		 case 2 : {
+    			 userName = "mother"; 
+		            break;  
+    		 }
+    		 
+    		 case 3: {
+    			 
+    			 userName = "spouse";
+		            break;  
+    		 }
+    		 case 4: {
+    			 
+    			 userName = "self";
+		            break;  
+    		 }
+    		 default : {
+    			    return;
+    		 }
+    			 }
+                     break;
+    		 }
+    		 
+    		 case 3 : {
+    			 userType = "Typhoid";
+    			         
+                               switch(chooseVaccinatePerson) {
+    			 
+    			 case 1:{
+    				          
+    				 userName   = "father";
+    			            break;
+    		 
+    			 }
+//    		 
+//    
+    		 case 2 : {
+    			 
+    			
+    			 userName = "mother";
+		            break;  
+    		 }
+//    		 
+    		 case 3: {
+
+    			 userName = "spouse";
+		            break;  
+    		 }
+    		 case 4: {
+    			 
+    			 userName = "self";
+		            break;  
+    		 }
+    		 default : {
+    			    return;
+    		         }
+    	     }
+    	 }
+    			 
+    			break;            
+    		 }
+    		 
+    		
+    		User user = (User) context.getBean(userName + userType); 
+    		
+    		        if(user.IsVaccinated()) {
+    		              System.out.println("User is already Vaccinated");
+    		         }
+    		                   
+    		         else {
+    		System.out.println("Please enter "+userName+" details");
+    	
+              
+    			System.out.print("Name: ");
+    		   String	name = scanner.nextLine();
+    			System.out.print("Age: ");
+    			int age = scanner.nextInt();
+
+    			scanner.nextLine();
+
+    			 System.out.print("Appointment date (YYYY-MM-DD): ");
+    			String userAppointmentDate = scanner.nextLine();
+    			 
+    			 System.out.print("Appointment time (HH:MM AM/PM): ");
+    			String userAppointmentTime = scanner.nextLine();
+    			 
+    			 System.out.print("Appointment Location : ");
+    			String userAppointmentLocation = scanner.nextLine();
+    		
+    		
+    		       
+    	//	user.setUserTimeLocationDetails(userAppointmentTime, userAppointmentLocation, userAppointmentDate)
+    		//  user.(userAppointmentTime, userAppointmentLocation, userAppointmentDate)
+    		// TimeAndLocation t1 =  null;
+    		TimeAndLocation t1 =
+    			    (TimeAndLocation) context.getBean("timeAndLocation");
+    		 t1.setDetails(userAppointmentTime, userAppointmentLocation, userAppointmentDate);
+    		 user.setUserDetails(name, age, t1);
+    		 user.getVaccineDetails();
+    		
+    		 
+    		    user.setAppointment();
+    		         }
+    		    System.out.println("Do you want to register for Someone Else\n1. Yes\n2. No");
+    		    int choice = scanner.nextInt();
+    		     if(choice == 1) {
+    		    	  continue;
+    		     }
+    		         
+    		     
+    		     break;
+    		         	
+    		 
+    	 }
+    	 scanner.close();
+    	 context.close();
+	     
+    	
+    }
+}
+
+
+
+
+
+
+
+
+
+
