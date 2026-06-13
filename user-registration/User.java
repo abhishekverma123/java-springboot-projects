45b935513f8b7e3604822fbd1e10dea7e8f0b565
ABHISHEK  VERMA <76154794+abhishekverma123@users.noreply.github.com>
2026-06-14 03:04:57 +0530

    Delete user-registration directory

--- user-registration/User.java
+++
@@ -1,7 +0,0 @@
-package com.example.Website.domain;
-
-public interface User {
-     // create a user method
-	public boolean createUser(String name,String gender, String location, String college);
-	public Integer saveUser();
-}
--- user-registration/UserServiceLayer.java
+++
@@ -1,9 +0,0 @@
-package com.example.Website.service;
-
-import com.example.Website.domain.User;
-
-public interface UserServiceLayer {
-	public User getUser();
-	public boolean signup(String name, String gender, String location, String college);
-	
-}
